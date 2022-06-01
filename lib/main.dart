8d7c694d1c137d5385c70c47bc4e095c7e924f6a
Manit Gandhi <tripleking123@gmail.com>
2022-06-02 00:39:03 +0700

    use image in app

--- lib/main.dart
+++ lib/main.dart
@@ -119,6 +119,39 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
               ),
+              Align(
+                alignment: AlignmentDirectional(-0.05, -0.05),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+                  child: Container(
+                    width: 250,
+                    height: 250,
+                    constraints: BoxConstraints(
+                      maxWidth: double.infinity,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFEEEEEE),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 5,
+                          color: Color(0x4D000000),
+                          offset: Offset(0, 2),
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+                      child: Image.asset(
+                        'assets/images/flash-logo.png',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
